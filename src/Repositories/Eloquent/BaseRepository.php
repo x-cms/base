061e4a28f3a037acdc9ef0d229a958ce1b0c298d
靳鸿杰 <jinhongjie@jinhongjiedeiMac.local>
2017-05-19 01:11:49 +0800

    add repository

---
+++ src/Repositories/Eloquent/BaseRepository.php
@@ -0,0 +1,642 @@
+<?php
+namespace Xcms\Base\Repository\Eloquent;
+
+use Closure;
+use Exception;
+use Illuminate\Container\Container as Application;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+
+use Xcms\Base\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Class BaseRepository
+ * @package Prettus\Repository\Eloquent
+ */
+abstract class BaseRepository implements RepositoryInterface
+{
+    /**
+     * @var Application
+     */
+    protected $app;
+
+    /**
+     * @var Model
+     */
+    protected $model;
+
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [];
+
+    /**
+     * @var \Closure
+     */
+    protected $scopeQuery = null;
+
+    /**
+     * @param Application $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+        $this->criteria = new Collection();
+        $this->makeModel();
+        $this->makeValidator();
+        $this->boot();
+    }
+
+    /**
+     *
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function resetModel()
+    {
+        $this->makeModel();
+    }
+
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    abstract public function model();
+
+    /**
+     * @return Model
+     * @throws Exception
+     */
+    public function makeModel()
+    {
+        $model = $this->app->make($this->model());
+
+        if (!$model instanceof Model) {
+            throw new Exception("Class {$this->model()} must be an instance of Illuminate\\Database\\Eloquent\\Model");
+        }
+
+        return $this->model = $model;
+    }
+
+    /**
+     * Get Searchable Fields
+     *
+     * @return array
+     */
+    public function getFieldsSearchable()
+    {
+        return $this->fieldSearchable;
+    }
+
+    /**
+     * Query Scope
+     *
+     * @param \Closure $scope
+     *
+     * @return $this
+     */
+    public function scopeQuery(\Closure $scope)
+    {
+        $this->scopeQuery = $scope;
+
+        return $this;
+    }
+
+    /**
+     * Retrieve data array for populate field select
+     *
+     * @param string $column
+     * @param string|null $key
+     *
+     * @return \Illuminate\Support\Collection|array
+     */
+    public function lists($column, $key = null)
+    {
+        return $this->model->lists($column, $key);
+    }
+
+    /**
+     * Retrieve data array for populate field select
+     * Compatible with Laravel 5.3
+     * @param string $column
+     * @param string|null $key
+     *
+     * @return \Illuminate\Support\Collection|array
+     */
+    public function pluck($column, $key = null)
+    {
+        return $this->model->pluck($column, $key);
+    }
+
+    /**
+     * Sync relations
+     *
+     * @param $id
+     * @param $relation
+     * @param $attributes
+     * @param bool $detaching
+     * @return mixed
+     */
+    public function sync($id, $relation, $attributes, $detaching = true)
+    {
+        return $this->find($id)->{$relation}()->sync($attributes, $detaching);
+    }
+
+    /**
+     * SyncWithoutDetaching
+     *
+     * @param $id
+     * @param $relation
+     * @param $attributes
+     * @return mixed
+     */
+    public function syncWithoutDetaching($id, $relation, $attributes)
+    {
+        return $this->sync($id, $relation, $attributes, false);
+    }
+
+    /**
+     * Retrieve all data of repository
+     *
+     * @param array $columns
+     *
+     * @return mixed
+     */
+    public function all($columns = ['*'])
+    {
+        $this->applyScope();
+
+        if ($this->model instanceof Builder) {
+            $results = $this->model->get($columns);
+        } else {
+            $results = $this->model->all($columns);
+        }
+
+        $this->resetModel();
+        $this->resetScope();
+
+        return $results;
+    }
+
+
+    /**
+     * Retrieve first data of repository
+     *
+     * @param array $columns
+     *
+     * @return mixed
+     */
+    public function first($columns = ['*'])
+    {
+        $this->applyScope();
+
+        $results = $this->model->first($columns);
+
+        $this->resetModel();
+
+        return $results;
+    }
+
+    /**
+     * Retrieve first data of repository, or return new Entity
+     *
+     * @param array $attributes
+     *
+     * @return mixed
+     */
+    public function firstOrNew(array $attributes = [])
+    {
+        $this->applyScope();
+        $model = $this->model->firstOrNew($attributes);
+        $this->resetModel();
+
+        return $model;
+    }
+
+    /**
+     * Retrieve first data of repository, or create new Entity
+     *
+     * @param array $attributes
+     *
+     * @return mixed
+     */
+    public function firstOrCreate(array $attributes = [])
+    {
+        $this->applyScope();
+        $model = $this->model->firstOrCreate($attributes);
+        $this->resetModel();
+
+        return $model;
+    }
+
+    /**
+     * Retrieve all data of repository, paginated
+     *
+     * @param null $limit
+     * @param array $columns
+     * @param string $method
+     *
+     * @return mixed
+     */
+    public function paginate($limit = null, $columns = ['*'], $method = "paginate")
+    {
+        $this->applyScope();
+        $limit = is_null($limit) ? config('repository.pagination.limit', 15) : $limit;
+        $results = $this->model->{$method}($limit, $columns);
+        $results->appends(app('request')->query());
+        $this->resetModel();
+
+        return $results;
+    }
+
+    /**
+     * Retrieve all data of repository, simple paginated
+     *
+     * @param null $limit
+     * @param array $columns
+     *
+     * @return mixed
+     */
+    public function simplePaginate($limit = null, $columns = ['*'])
+    {
+        return $this->paginate($limit, $columns, "simplePaginate");
+    }
+
+    /**
+     * Find data by id
+     *
+     * @param       $id
+     * @param array $columns
+     *
+     * @return mixed
+     */
+    public function find($id, $columns = ['*'])
+    {
+        $this->applyScope();
+        $model = $this->model->findOrFail($id, $columns);
+        $this->resetModel();
+
+        return $model;
+    }
+
+    /**
+     * Find data by field and value
+     *
+     * @param       $field
+     * @param       $value
+     * @param array $columns
+     *
+     * @return mixed
+     */
+    public function findByField($field, $value = null, $columns = ['*'])
+    {
+        $this->applyScope();
+        $model = $this->model->where($field, '=', $value)->get($columns);
+        $this->resetModel();
+
+        return $model;
+    }
+
+    /**
+     * Find data by multiple fields
+     *
+     * @param array $where
+     * @param array $columns
+     *
+     * @return mixed
+     */
+    public function findWhere(array $where, $columns = ['*'])
+    {
+        $this->applyScope();
+
+        $this->applyConditions($where);
+
+        $model = $this->model->get($columns);
+        $this->resetModel();
+
+        return $model;
+    }
+
+    /**
+     * Find data by multiple values in one field
+     *
+     * @param       $field
+     * @param array $values
+     * @param array $columns
+     *
+     * @return mixed
+     */
+    public function findWhereIn($field, array $values, $columns = ['*'])
+    {
+        $this->applyScope();
+        $model = $this->model->whereIn($field, $values)->get($columns);
+        $this->resetModel();
+
+        return $model;
+    }
+
+    /**
+     * Find data by excluding multiple values in one field
+     *
+     * @param       $field
+     * @param array $values
+     * @param array $columns
+     *
+     * @return mixed
+     */
+    public function findWhereNotIn($field, array $values, $columns = ['*'])
+    {
+        $this->applyScope();
+        $model = $this->model->whereNotIn($field, $values)->get($columns);
+        $this->resetModel();
+
+        return $model;
+    }
+
+    /**
+     * Save a new entity in repository
+     *
+     * @throws Exception
+     *
+     * @param array $attributes
+     *
+     * @return mixed
+     */
+    public function create(array $attributes)
+    {
+        if (!is_null($this->validator)) {
+            // we should pass data that has been casts by the model
+            // to make sure data type are same because validator may need to use
+            // this data to compare with data that fetch from database.
+            $attributes = $this->model->newInstance()->forceFill($attributes)->toArray();
+
+            $this->validator->with($attributes)->passesOrFail(ValidatorInterface::RULE_CREATE);
+        }
+
+        $model = $this->model->newInstance($attributes);
+        $model->save();
+        $this->resetModel();
+
+        event(new RepositoryEntityCreated($this, $model));
+
+        return $this->parserResult($model);
+    }
+
+    /**
+     * Update a entity in repository by id
+     *
+     * @throws ValidatorException
+     *
+     * @param array $attributes
+     * @param       $id
+     *
+     * @return mixed
+     */
+    public function update(array $attributes, $id)
+    {
+        $this->applyScope();
+
+        if (!is_null($this->validator)) {
+            // we should pass data that has been casts by the model
+            // to make sure data type are same because validator may need to use
+            // this data to compare with data that fetch from database.
+            $attributes = $this->model->newInstance()->forceFill($attributes)->toArray();
+
+            $this->validator->with($attributes)->setId($id)->passesOrFail(ValidatorInterface::RULE_UPDATE);
+        }
+
+        $temporarySkipPresenter = $this->skipPresenter;
+
+        $model = $this->model->findOrFail($id);
+        $model->fill($attributes);
+        $model->save();
+
+        $this->resetModel();
+
+        event(new RepositoryEntityUpdated($this, $model));
+
+        return $model;
+    }
+
+    /**
+     * Update or Create an entity in repository
+     *
+     * @throws ValidatorException
+     *
+     * @param array $attributes
+     * @param array $values
+     *
+     * @return mixed
+     */
+    public function updateOrCreate(array $attributes, array $values = [])
+    {
+        $this->applyScope();
+
+        if (!is_null($this->validator)) {
+            $this->validator->with($attributes)->passesOrFail(ValidatorInterface::RULE_UPDATE);
+        }
+
+        $model = $this->model->updateOrCreate($attributes, $values);
+        $this->resetModel();
+
+        event(new RepositoryEntityUpdated($this, $model));
+
+        return $model;
+    }
+
+    /**
+     * Delete a entity in repository by id
+     *
+     * @param $id
+     *
+     * @return int
+     */
+    public function delete($id)
+    {
+        $this->applyScope();
+        $model = $this->find($id);
+        $originalModel = clone $model;
+        $this->resetModel();
+        $deleted = $model->delete();
+
+        event(new RepositoryEntityDeleted($this, $originalModel));
+
+        return $deleted;
+    }
+
+    /**
+     * Delete multiple entities by given criteria.
+     *
+     * @param array $where
+     *
+     * @return int
+     */
+    public function deleteWhere(array $where)
+    {
+        $this->applyScope();
+        $this->applyConditions($where);
+        $deleted = $this->model->delete();
+
+        event(new RepositoryEntityDeleted($this, $this->model->getModel()));
+
+        $this->resetModel();
+
+        return $deleted;
+    }
+
+    /**
+     * Check if entity has relation
+     *
+     * @param string $relation
+     *
+     * @return $this
+     */
+    public function has($relation)
+    {
+        $this->model = $this->model->has($relation);
+
+        return $this;
+    }
+
+    /**
+     * Load relations
+     *
+     * @param array|string $relations
+     *
+     * @return $this
+     */
+    public function with($relations)
+    {
+        $this->model = $this->model->with($relations);
+
+        return $this;
+    }
+
+    /**
+     * Add subselect queries to count the relations.
+     *
+     * @param  mixed $relations
+     * @return $this
+     */
+    public function withCount($relations)
+    {
+        $this->model = $this->model->withCount($relations);
+        return $this;
+    }
+
+    /**
+     * Load relation with closure
+     *
+     * @param string $relation
+     * @param closure $closure
+     *
+     * @return $this
+     */
+    public function whereHas($relation, $closure)
+    {
+        $this->model = $this->model->whereHas($relation, $closure);
+
+        return $this;
+    }
+
+    /**
+     * Set hidden fields
+     *
+     * @param array $fields
+     *
+     * @return $this
+     */
+    public function hidden(array $fields)
+    {
+        $this->model->setHidden($fields);
+
+        return $this;
+    }
+
+    public function orderBy($column, $direction = 'asc')
+    {
+        $this->model = $this->model->orderBy($column, $direction);
+
+        return $this;
+    }
+
+    /**
+     * Set visible fields
+     *
+     * @param array $fields
+     *
+     * @return $this
+     */
+    public function visible(array $fields)
+    {
+        $this->model->setVisible($fields);
+
+        return $this;
+    }
+
+    /**
+     * Reset Query Scope
+     *
+     * @return $this
+     */
+    public function resetScope()
+    {
+        $this->scopeQuery = null;
+
+        return $this;
+    }
+
+    /**
+     * Apply scope in current Query
+     *
+     * @return $this
+     */
+    protected function applyScope()
+    {
+        if (isset($this->scopeQuery) && is_callable($this->scopeQuery)) {
+            $callback = $this->scopeQuery;
+            $this->model = $callback($this->model);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Applies the given where conditions to the model.
+     *
+     * @param array $where
+     * @return void
+     */
+    protected function applyConditions(array $where)
+    {
+        foreach ($where as $field => $value) {
+            if (is_array($value)) {
+                list($field, $condition, $val) = $value;
+                $this->model = $this->model->where($field, $condition, $val);
+            } else {
+                $this->model = $this->model->where($field, '=', $value);
+            }
+        }
+    }
+
+    /**
+     * Cast HTTP request object to an array if need be.
+     *
+     * @param array|Request $request
+     *
+     * @return array
+     */
+    protected function castRequest($request)
+    {
+        return $request instanceof Request ? $request->all() : $request;
+    }
+}
